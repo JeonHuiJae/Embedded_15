24e8eac82824c565560378952e03d1ce83ac7c4d
cacao518 <qudwns518@naver.com>
2019-12-08 19:51:33 +0900

    텔레그램 플라스크 연동

--- DropBox.py
+++ DropBox.py
@@ -14,6 +14,7 @@ led = 20 # 표시등
 Time = 0 # 감지용 타임변수
 Mode = 0 # 0: 감지상태 1: 도난방지 2: 도난 3: 수령
 FirstDetect = False # 처음감지됨 표시ㅇㅇ
+x = 0
 
 #- mav setting
 pygame.mixer.init()
@@ -28,9 +29,12 @@ app = Flask(__name__)
 
 def Send(Message):
     telegram_id = '1020899593' #아이디 입력
-    my_token = '868641147:AAGt-S98GEBY3PcKc7GmMty16RL7UhkUX4w' #token 입력
+    telegram_id2 = '929570079' #아이디 입력
+    my_token = '1016022560:AAEaEHoJxgyqpqm_vXpDCnE_TrZhKM3sMy4' #token 입력
     bot = telepot.Bot(my_token) #봇을 생성해줍니다.
     bot.sendMessage(chat_id = telegram_id, text = Message) #메시지를 보냅니다.
+    bot.sendMessage(chat_id = telegram_id2, text = Message) #메시지를 보냅니다.
+
 
 
 class AsyncTask:
@@ -50,9 +54,9 @@ class AsyncTask:
             Time += 1
             print(Time)
             if Time == 5:
-                Mode = 1 # 도난방지모드
                 threading.Timer(1,self.Shot).start()
-                print("도난방지모드로 전환되었습니다.")
+                print("물체가 감지되었습니다.")
+                Send("물체가 감지되었습니다. http://203.252.166.155/detect")
                 Time = 0
             else:
                 threading.Timer(1,self.Detector).start()
@@ -75,18 +79,75 @@ class AsyncTask:
                 Time = 0
                 break
                 
+                
     def WebRun(self):
         global app
         app.run(host='0.0.0.0', port=80, debug=False, threaded=True)
 
 
-@app.route("/")
-def page():
-    message = "환영합니다."
+@app.route("/detect")
+def detect():
+    message = "물체가 감지되었습니다.\n택배가 맞나요?"
     templateData = {
         'message' : message,
     }
-    return render_template('DropBox.html', **templateData)
+    return render_template('DetectMode.html', **templateData)
+
+@app.route("/message/<action>")
+def action(action):
+    global x, Mode
+
+    if action == "receive" :
+        message = "수령처리되었습니다."
+        Mode = 0
+    else :
+        if x != 1 :
+            message = "택배가 아닌것으로 처리되었습니다."
+            Mode = 0
+        else :
+            message = "그 사이에 물체가 사라졌습니다."
+        
+    templateData = {
+        'message' : message,
+    }    
+    return render_template('Message.html', **templateData)
+
+@app.route("/protect")
+def protect():
+    global Mode
+    if x != 1 :
+        Mode = 1 # 도난방지모드
+        print("도난방지모드로 전환되었습니다.")
+        Send("택배를 보호중입니다. http://203.252.166.155/protect")    
+        message = "택배를 보호중입니다."
+        templateData = {
+            'message' : message,
+        }
+        return render_template('ProtectMode.html', **templateData)
+    else :
+        message = "그 사이에 물체가 사라졌습니다."
+        templateData = {
+            'message' : message,
+        }
+        return render_template('Message.html', **templateData)
+            
+@app.route("/robbed")
+def robbed():
+    message = "도난이 감지되었습니다!!"
+    templateData = {
+        'message' : message,
+    }
+    return render_template('RobbedMode.html', **templateData)
+
+@app.after_request
+def add_header(response):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    response.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
+    response.headers['Cache-Control'] = 'public, max-age=0'
+    return response
 
 if __name__ == '__main__':
     at = AsyncTask()
@@ -98,7 +159,6 @@ if __name__ == '__main__':
             if x != 1 : # 감지 O
                 if FirstDetect == False and Mode == 0: # 처음감지
                     print("물체가 감지되었습니다.")
-                    Send("물체가 감지되었습니다.")
                     at.Detector() # 10초 카운트
                     FirstDetect = True
                 GPIO.output(led, GPIO.HIGH)
@@ -108,7 +168,7 @@ if __name__ == '__main__':
                 if Mode == 1: # 도난방지모드일때 감지X == 도난
                     Mode = 2 # 도난
                     print("도난이 감지되었습니다..!")
-                    Send("도난이 감지되었습니다..!")
+                    Send("도난이 감지되었습니다..! http://203.252.166.155/robbed")
                     threading.Timer(1,at.Shot).start()
                     at.Alert()
                     Mode = 0 # 도난감지모드
@@ -122,25 +182,4 @@ if __name__ == '__main__':
         camera.close()
 
 
-            
-#- web 제어
-# type 택배감지됨:detect/ 감지모드 작동:detectM/ 도난경보:alert/
-# 택배맞음?:isBox / 도난맞음?:isRobbed/수령버튼 :receipt
-
-@app.route("/<page>")
-def action(page):
-    global detectMode, robbed, isReceipt
-    if page == 'isRight':
-        if response == 'Y':
-            detectMode = True
-            message = "택배가 맞나요?"
-            
-    if page == 'Anti-theft':
-            message = "도난방지모드 입니다"
-    if page == 'Robbed':
-        message = "도난이 감지되었습니다"
         
-    templateData = {
-        'message' : message,
-    }
-    return render_template('DropBox.html', **templateData)
