95fbc32039fac313f912aaaaf233113f22f3b818
JeonHuiJae <huijae0817@gmail.com>
2019-12-08 16:15:59 +0900

    복구완료

--- DropBox.py
+++ DropBox.py
@@ -12,9 +12,6 @@ Time = 0 # 감지용 타임변수
 Mode = 0 # 0: 감지상태 1: 도난방지 2: 도난 3: 수령
 FirstDetect = False # 처음감지됨 표시
 
-#- camera setting
-camera = picamera.PiCamera()
-camera.resolution = (800, 600)
 #- mav setting
 pygame.mixer.init()
 beep = pygame.mixer.Sound("beep3.wav")
@@ -23,16 +20,20 @@ GPIO.setup(irpin, GPIO.IN)
 GPIO.setup(led, GPIO.OUT)
 GPIO.output(led, GPIO.LOW)
 
-def Shot():
-    now = time.localtime()
-    fileName = "%04d-%02d-%02d %02d:%02d:%02d.jpg" % (now.tm_year, now.tm_mon, now.tm_mday,now.tm_hour, now.tm_min, now.tm_sec)
-    with picamera.PiCamera() as camera:
-        camera.capture('picture/'+fileName)
-    time.sleep(1)
-    print("사진 저장.")
+app = Flask(__name__)
+
+
 
 class AsyncTask:
-    
+    def Shot(self):
+        camera = picamera.PiCamera()
+        camera.resolution = (800, 600)
+        camera.capture('picture/ex1.jpg')
+        time.sleep(0.1)
+        camera.close()
+
+        #return render_template("DropBox.html")
+
     def Detector(self):
         global Mode
         if x != 1:
@@ -41,29 +42,35 @@ class AsyncTask:
             print(Time)
             if Time == 5:
                 Mode = 1 # 도난방지모드
-                Shot()
+                threading.Timer(1,self.Shot).start()
                 print("도난방지모드로 전환되었습니다.")
                 Time = 0
             else:
                 threading.Timer(1,self.Detector).start()
         else:
             Time =0
+            Mode = 0
 
     def Alert(self):
         global respond, robbed, Time
+        Time = 0;
+        beep.play()
         while True:
             Time += 1
-            beep.play()
-            time.sleep(1.0)
+            time.sleep(0.2)
             GPIO.output(led, GPIO.HIGH)
-            time.sleep(1)
+            time.sleep(0.2)
             GPIO.output(led, GPIO.LOW)
-            time.sleep(1)
-            if Time == 3:
+            time.sleep(0.2)
+            if Time == 8:
                 Time = 0
                 break
+                
+    def WebRun(self):
+        global app
+        app.run(host='0.0.0.0', port=80, debug=False, threaded=True)
 
-app = Flask(__name__)
+
 @app.route("/")
 def page():
     message = "환영합니다."
@@ -73,30 +80,29 @@ def page():
     return render_template('DropBox.html', **templateData)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80, debug=False, threaded=True)
     at = AsyncTask()
     try:
+        threading.Timer(1,at.WebRun).start()
         while True:
             x = GPIO.input(irpin)
             time.sleep(0.1)
             if x != 1 : # 감지 O
-                if firstDetect == True and Mode == 0: # 처음감지
+                if FirstDetect == False and Mode == 0: # 처음감지
                     print("물체가 감지되었습니다.")
                     at.Detector() # 10초 카운트
-                    firstDetect = False
-                else:
-                    GPIO.output(led, GPIO.HIGH)
+                    FirstDetect = True
+                GPIO.output(led, GPIO.HIGH)
+                    
             else: # 감지 X
                 Time = 0
                 if Mode == 1: # 도난방지모드일때 감지X == 도난
                     Mode = 2 # 도난
                     print("도난이 감지되었습니다..!")
-                    Shot()
+                    threading.Timer(1,at.Shot).start()
                     at.Alert()
                     Mode = 0 # 도난감지모드
-                else:
-                    GPIO.output(led, GPIO.LOW)
-                    firstDetect = True
+                GPIO.output(led, GPIO.LOW)
+                FirstDetect = False
 
                         
     except KeyboardInterrupt:
